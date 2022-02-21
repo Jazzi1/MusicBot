e928975ad0a4bb47f070873b1ae01cd91afe2439
Jazzi1 <kalaev.rus@mail.ru>
2022-02-21 20:24:10 +0300

    Добавил команду для отправления конкретной музыки пользователю

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from telegram.ext import Updater, CallbackContext, CommandHandler
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 from telegram import Update
 from mutagen.mp3 import MP3
-from musicfunctions import get_random_music
+from musicfunctions import get_random_music, get_current_music
 
 
 def start_command(update: Update, _: CallbackContext):
     update.effective_chat.send_message(text='Привет!')
 
 
-def send_random_audio_command(update: Update, _: CallbackContext):
+def send_audio(update: Update, music_name: str):
     update.effective_chat.send_audio(
-        audio=open(get_random_music(full_path=True), 'rb'),
-        duration=int(MP3(get_random_music(full_path=True)).info.length)
+        audio=open(music_name, 'rb'),
+        duration=int(MP3(music_name).info.length)
     )
+
+
+def send_random_audio_command(update: Update, _: CallbackContext):
+    send_audio(update, get_random_music(full_path=True))
+
+
+def get_current_music_command(update: Update, _: CallbackContext):
+    message = update.effective_message.text
+    send_audio(update, get_current_music(message, full_path=True))
 
 
 def main(token: str) -> None:
@@ -22,6 +31,7 @@ def main(token: str) -> None:
 
     dispatcher.add_handler(CommandHandler('start', start_command))
     dispatcher.add_handler(CommandHandler('send_random_audio', send_random_audio_command))
+    dispatcher.add_handler(MessageHandler(Filters.text, get_current_music_command))
 
     print(updater.bot.get_me())
     updater.start_polling()
@@ -33,4 +43,3 @@ if __name__ == '__main__':
         m = t.read()
 
     main(m)
-
--- musicfunctions.py
+++ musicfunctions.py
@@ -15,3 +15,16 @@ def get_random_music(full_path=False):
     if full_path:
         music_name = MUSIC_DIR + music_name
     return music_name
+
+
+def get_current_music(name_music: str, full_path=False):
+    name_music = name_music.lower()
+    music_list = list(map(lambda x: x.split('.')[0].lower(), get_music_list()))
+
+    music_index = music_list.index(name_music)
+    music_name = get_music_list()[music_index]
+
+    if full_path:
+        music_name = MUSIC_DIR + music_name
+
+    return music_name
